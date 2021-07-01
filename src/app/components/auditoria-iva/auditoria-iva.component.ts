01583eec0b61482531485737d36c5d16aaca893b
asdelaf <asanchezdelaf@hotmail.com>
2021-07-01 19:00:28 -0300

    excel y pdf auditorias

--- src/app/components/auditoria-iva/auditoria-iva.component.ts
+++ src/app/components/auditoria-iva/auditoria-iva.component.ts
@@ -4,6 +4,8 @@ import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { AuditoriaIVAService } from 'src/app/services/auditoria-iva.service';
 import { EmpleadoService } from 'src/app/services/empleado.service';
+import { ExcelService } from 'src/app/services/excel.service';
+import * as XLSX from 'xlsx'; 
 
 @Component({
   selector: 'app-auditoria-iva',
@@ -13,6 +15,7 @@ import { EmpleadoService } from 'src/app/services/empleado.service';
 export class AuditoriaIvaComponent implements OnInit {
 
   auditoriaIVA: any[] = [];
+  fileName= 'AuditoriaIVA.xlsx';
 
   constructor(private _auditoriaIVAService: AuditoriaIVAService,
               private toastr: ToastrService) {
@@ -20,6 +23,21 @@ export class AuditoriaIvaComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAuditoriaIVA()
+  }
+
+  exportexcel(): void 
+  {
+     /* table id is passed over here */   
+     let element = document.getElementById('excel-table'); 
+     const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+
+     /* generate workbook and add the worksheet */
+     const wb: XLSX.WorkBook = XLSX.utils.book_new();
+     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+     /* save to file */
+     XLSX.writeFile(wb, this.fileName);
+    
   }
 
   getAuditoriaIVA() {
--- src/app/components/auditoria/auditoria.component.ts
+++ src/app/components/auditoria/auditoria.component.ts
@@ -4,6 +4,8 @@ import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { AuditoriaClientesService } from 'src/app/services/auditoria-clientes.service';
 import { EmpleadoService } from 'src/app/services/empleado.service';
+import { ExcelService } from 'src/app/services/excel.service';
+import * as XLSX from 'xlsx'; 
 
 @Component({
   selector: 'app-auditoria',
@@ -13,6 +15,7 @@ import { EmpleadoService } from 'src/app/services/empleado.service';
 export class AuditoriaComponent implements OnInit {
 
   auditoriaclientes: any[] = [];
+  fileName= 'AuditoriaClientes.xlsx';
 
   constructor(private _auditoriaClientesService: AuditoriaClientesService,
               private toastr: ToastrService) {
@@ -20,6 +23,21 @@ export class AuditoriaComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAuditoriaClientes()
+  }
+
+  exportexcel(): void 
+  {
+     /* table id is passed over here */   
+     let element = document.getElementById('excel-table'); 
+     const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+
+     /* generate workbook and add the worksheet */
+     const wb: XLSX.WorkBook = XLSX.utils.book_new();
+     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+     /* save to file */
+     XLSX.writeFile(wb, this.fileName);
+    
   }
 
   getAuditoriaClientes() {
