dfbc756f4588a3ec1e04eea66e4bb7495e2b44ee
DiegoSchenk <diegoschenk@gmail.com>
2021-06-30 21:05:53 -0300

    Creando servicio de auditoria

---
+++ src/app/services/auditoria-clientes.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuditoriaClientesService {
+
+  constructor(private firestore: AngularFirestore) { }
+
+  agregarAuditoriaClientes(auditoriaClientes: any): Promise<any> {
+    return this.firestore.collection('auditoriaClientes').add(auditoriaClientes);
+  }
+
+  getAuditoriaClientes(): Observable<any> {
+    return this.firestore.collection('auditoriaClientes', ref => ref.orderBy('fechaCreacion', 'asc')).snapshotChanges();
+  }
+
+  actualizarAuditoriaClientes(id: string, data:any): Promise<any> {
+    return this.firestore.collection('auditoriaClientes').doc(id).update(data);
+  }
+
+}
