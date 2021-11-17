20399e1e0c5d211d1c687f70f8a7fb75da9ac4d2
DiegoSchenk <diegoschenk@gmail.com>
2021-11-17 20:41:36 -0300

    New router links!

--- src/app/components/contaduria/contaduria.component.ts
+++ src/app/components/contaduria/contaduria.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthGuard } from 'src/app/guards/auth.guard';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contaduria',
@@ -8,9 +9,15 @@ import { AuthGuard } from 'src/app/guards/auth.guard';
 })
 export class ContaduriaComponent implements OnInit {
 
-  constructor(public guard:AuthGuard) { }
+  constructor(public guard:AuthGuard, private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  cambiarRouter(num:number){
+    if(num == 1) this.router.navigate(['asientos']);
+    else if (num == 2) this.router.navigate(['balance-general']);
+    else if (num == 3) this.router.navigate(['librodiario']);
+    else this.router.navigate(['mayordiario']);
+  }
 }
